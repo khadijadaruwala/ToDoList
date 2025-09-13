a96dfc09064392b36bbfb7f1319b09b445df6119
Khadija Daruwala <khadija.genius@gmail.com>
2025-09-12 22:18:25 -0400

    Initial project set up

---
+++ TodoList/ToDoList.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoList.swift
+//  TodoList
+//
+//  Created by Khadija Daruwala on 2025-09-11.
+//
+
+import SwiftUI
+
+struct ToDoList: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ToDoList()
+}
---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Khadija Daruwala on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ToDoList()
+        }
+    }
+}
